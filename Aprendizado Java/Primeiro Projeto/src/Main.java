995dc4f17f834c3fd1bed670ee1b7c50675fa92f
Pedro <ppchakur@gmail.com>
2026-03-23 16:48:57 -0300

    .

--- Aprendizado Java/Primeiro Projeto/src/Main.java
+++ Aprendizado Java/Primeiro Projeto/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top GUn: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: "+ anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }
--- Aprendizado Java/Segundo Projeto/src/perfil.java
+++
@@ -1,6 +0,0 @@
-public class perfil {
-    static void main(String[] args) {
-        System.out.println("Olá, Pedro");
-        System.out.println("Tudo bem?");
-    }
-}

235028a03e7a4b24495129fbf7aed599707cdf43
Pedro <ppchakur@gmail.com>
2026-03-23 17:06:23 -0300

    .

--- Aprendizado Java/Primeiro Projeto/src/Main.java
+++ Aprendizado Java/Primeiro Projeto/src/Main.java
@@ -7,5 +7,19 @@ public class Main {
         System.out.println("Ano de lançamento: "+ anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0)/3;
+        System.out.println(media);
+        String Sinopse="Filme Top Gun com ator famoso dos anos 90";
+        //podemos fazer também:
+        String sinopse = """
+                Filme Top Gun com ator famoso dos anos 90
+                
+                Ótimo filme!
+                
+                Ano de lançamento
+                """+ anoDeLancamento;
+        System.out.println(sinopse);
+
     }
 }
